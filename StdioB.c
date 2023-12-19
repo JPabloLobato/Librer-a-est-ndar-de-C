41d20ef2db7889cb7aea27a9b00ec66401e0d5db
JPabloLobato <jlobagon@myuax.com>
2023-12-19 09:33:23 +0100

    commit

--- StdioB.c
+++ StdioB.c
@@ -1,3 +1,36 @@
-//
-// Created by Pablo Lobato on 19/12/2023.
-//
+#include <stdio.h>
+
+int main() {
+    // Escribir en el archivo.
+    FILE *archivo_escritura = fopen("file.txt", "w");
+    if (archivo_escritura == NULL) {
+        printf("El archivo no se pudo abrir para escritura.\n");
+        return 1;
+    }
+
+    fprintf(archivo_escritura, "Hola, mundo!\n");
+
+    // Cerrar el archivo.
+    fclose(archivo_escritura);
+
+    // Leer archivo.
+    FILE *archivo_lectura = fopen("file.txt", "r");
+    if (archivo_lectura == NULL) {
+        printf("El archivo no se pudo abrir para lectura.\n");
+        return 1;
+    }
+
+    // Utilizar fscanf() para leer una línea.
+    char buffer[50];
+    if (fscanf(archivo_lectura, "%49[^\n]", buffer) == 1) {
+        // Imprimir lo leído del archivo
+        printf("Leído del archivo: %s\n", buffer);
+    } else {
+        printf("No se pudo leer correctamente del archivo.\n");
+    }
+
+    // Cerrar.
+    fclose(archivo_lectura);
+
+    return 0;
+}
--- stdioA.c
+++ stdioA.c
@@ -1,3 +1,14 @@
-//
-// Created by Pablo Lobato on 19/12/2023.
-//
+#include <stdio.h>
+
+int main() {
+    // Intentar abrir el archivo en modo de lectura
+    FILE *file = fopen("file.txt", "r");
+    // Verificar si el archivo se abrió correctamente.
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir.\n");
+        return 1;  // Salir del programa con un código de error
+    }
+    // Cerrar el archivo.
+    fclose(file);
+    return 0;
+}
